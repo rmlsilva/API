8b5def1780fe9f651bb1bdd6bb839b3091d25f2f
romulo <rmlsilva.dev@gmail.com>
2023-09-30 22:00:12 -0300

    Ajuste finais e instalacao do Cors

--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
-require('express-async-errors')
-const migrationsRun = require('./database/sqlite/migrations')
-const AppError = require('./utils/AppError')
+require('express-async-errors');
+const migrationsRun = require('./database/sqlite/migrations');
+const AppError = require('./utils/AppError');
+const uploadConfig = require("./configs/upload");
 
+const cors = require("cors");
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
 
-migrationsRun()
+migrationsRun();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(routes)
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
+
+app.use(routes);
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
