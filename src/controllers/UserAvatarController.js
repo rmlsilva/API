3d89d2fa3214230e1ef90bb308c062d40ebfb0ea
romulo <rmlsilva.dev@gmail.com>
2023-10-15 10:08:37 -0300

    adjust

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -2,32 +2,31 @@ const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const DiskStorage = require("../providers/DiskStorage");
 
-class UserAvatarController {
+export class UserAvatarController {
   async update(request, response) {
     const user_id = request.user.id;
-    const avatarFileName = request.file.filename;
-
-    const DiskStorage = new DiskStorage();
+    const avatarFilename = request.file.filename;
+    const diskStorage = new DiskStorage();
 
     const user = await knex("users").where({ id: user_id }).first();
 
     if (!user) {
       throw new AppError(
-        "Somente o usuário autenticado pode atualizar o avatar"
+        "Somente usuários autentificados podem mudar o avatar",
+        401
       );
     }
-
     if (user.avatar) {
-      await DiskStorage.deleteFile(user.avatar);
+      await diskStorage.deleteFile(user.avatar);
     }
 
-    const fileName = await DiskStorage.saveFile(avatarFileName);
-    user.avatar = fileName;
+    const filename = await diskStorage.saveFile(avatarFilename);
+    user.avatar = filename;
 
-    await knex("users").update(user).where({ id: user_id });
+    await knex("users").update(user).where({
+      id: user_id,
+    });
 
     return response.json(user);
   }
 }
-
-module.exports = UserAvatarController;
